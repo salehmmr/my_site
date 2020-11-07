191384d3630e7846923ff9d45650031c65216c71
saleh-mmr <saleh_mmr@yahoo.com>
2020-11-07 16:31:20 +0330

    a simple template with dynamic context, viwes, urls, models added

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
 ]
--- my_site/urls.py
+++ my_site/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
+    url(r'admin/', admin.site.urls),
+    url(r'catalog/', include('catalog.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
