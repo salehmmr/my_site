bf7d5d9b3676abf7ee46f918fbf23726832a86a9
saleh-mmr <saleh_mmr@yahoo.com>
2020-11-07 15:23:43 +0330

    Just Create a Project

---
+++ catalog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+]
